7450813c26c3fb96d4ce75ff26166bdce9a00347
Lucia Pereyra <lumes77iza@gmail.com>
2022-03-29 22:08:52 -0300

    subiendo nueva carpeta (faltan cosas)

---
+++ HolaSpringFrameWork/src/main/java/com/domain/controller/IndexController.java
@@ -0,0 +1,38 @@
+package com.domain.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/home")
+    public String goIndex() {
+    	
+		return "Index";
+    }
+	
+	@RequestMapping("/")
+    public String goPresentacion() {
+    	
+		return "Presentacion";
+    }
+	
+	@RequestMapping("/listado")
+	public String goListado(Model model) {
+		
+		List<String> alumnos = new ArrayList<String>();
+		alumnos.add("Juan");
+		alumnos.add("Pedro");
+		alumnos.add("José");
+		model.addAttribute("titulo", "Listado de alumnos");
+		model.addAttribute("profesor", "Gabriel Casas");
+		model.addAttribute("alumnos", alumnos);
+
+		return "Listado";
+	}
+}
---
+++ HolaSpringFrameWork/src/main/webapp/WEB-INF/views/Listado.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>${titulo}</title>
+</head>
+<body>
+  <h1> Profesor ${profesor} </h1>
+  
+  <ul>
+    <c:forEach items="${alumnos}" var="alumno">
+    <li><c:out value="${alumno}" /></li>
+    </c:forEach>
+  </ul>
+</body>
+</html>
