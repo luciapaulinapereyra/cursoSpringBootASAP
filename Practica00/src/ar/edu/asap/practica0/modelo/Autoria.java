e198beae35c74d36db65fa6db151ce7addd773c4
Lucia Pereyra <lumes77iza@gmail.com>
2022-04-05 14:45:11 -0300

    hasta el punto 3 de practica 2 juego

---
+++ Practica00/src/ar/edu/asap/practica0/modelo/Autoria.java
@@ -0,0 +1,32 @@
+package ar.edu.asap.practica0.modelo;
+
+import java.util.Date;
+
+public class Autoria {
+	private  Date fecha;
+	private int cantidadJugadas;
+	public Autoria() {
+		
+	}
+	public Autoria(Date fecha, int cantidadJugadas) {
+		super();
+		this.fecha = fecha;
+		this.cantidadJugadas = cantidadJugadas;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public int getCantidadJugadas() {
+		return cantidadJugadas;
+	}
+	public void setCantidadJugadas(int cantidadJugadas) {
+		this.cantidadJugadas = cantidadJugadas;
+	}
+   
+	public void contarDespuesDeLaJugada() {
+		
+	}
+}
