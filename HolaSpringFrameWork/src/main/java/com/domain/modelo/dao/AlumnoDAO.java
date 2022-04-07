97426379aa80adba91f41cb7ce62f6abe10bfb28
Lucia Pereyra <lumes77iza@gmail.com>
2022-04-07 15:52:59 -0300

    Haciendo operaciones CRUD

--- HolaSpringFrameWork/src/main/java/com/domain/modelo/dao/AlumnoDAO.java
+++ HolaSpringFrameWork/src/main/java/com/domain/modelo/dao/AlumnoDAO.java
@@ -2,6 +2,7 @@ package com.domain.modelo.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -38,19 +39,50 @@ public class AlumnoDAO implements DAO {
 
 	@Override
 	public void modificar(Model pModel) throws ClassNotFoundException,SQLException {
-
-
+		StringBuilder sql = new StringBuilder("UPDATE alumnos SET ALU_NOMBRE = ?, ALU_APELLIDO= ?, ALU_EMAIL=?, ALU_CONOCIMIENTOS=?, ALU_GIT= ? WHERE ALU_ID=?");
+		Alumno aluMod = (Alumno) pModel;
+		PreparedStatement stm = conexion.prepareStatement(sql.toString());
+		
+		stm.setString(1, aluMod.getNombre());
+		stm.setString(2, aluMod.getApellido());
+		stm.setString(3, aluMod.getEmail());
+		stm.setString(4, aluMod.getEstudios());
+		stm.setString(5, aluMod.getLinkArepositorio());
+		stm.setLong(6, aluMod.getCodigo());
+		
+		stm.executeUpdate();
 	}
 
 	@Override
-	public void eliminar(Model pModel) {
-
-
+	public void eliminar(Model pModel) throws SQLException {
+		StringBuilder sql = new StringBuilder("DELETE FROM alumnos WHERE ALU_ID=?");
+		Alumno aluMod = (Alumno) pModel;
+		PreparedStatement stm = conexion.prepareStatement(sql.toString());
+		stm.setLong(1, aluMod.getCodigo());
+		stm.executeUpdate();
 	}
 
 	@Override
-	public List<Model> leer(Model pModel) {
+	public List<Model> leer(Model pModel) throws SQLException {
+		ResultSet res;
+		StringBuilder sql = new StringBuilder("SELECT*FROM alumnos WHERE ALU_ID=?");
+		Alumno aluMod = (Alumno) pModel;
+		PreparedStatement stm = conexion.prepareStatement(sql.toString());
+		stm.setLong(1, aluMod.getCodigo());
+		
+		res= stm.executeQuery();
+		if(res.next()) {
+		   aluMod.setNombre(res.getString("ALU_NOMBRE"));
+		   aluMod.setApellido(res.getString("ALU_APELLIDO"));
+		   aluMod.setEmail(res.getString("ALU_EMAIL"));
+		   aluMod.setEstudios(res.getString("ALU_CONOCIMIENTOS"));
+		} else {
+			System.out.println("no existe esa persona");
+		}
 
+		
+		
+		System.out.println(aluMod);
 		return null;
 	}
 
--- HolaSpringFrameWork/src/test/java/com/domain/modelo/dao/test/AlumnoDAOTest.java
+++ HolaSpringFrameWork/src/test/java/com/domain/modelo/dao/test/AlumnoDAOTest.java
@@ -15,33 +15,75 @@ class AlumnoDAOTest {
 	
 	AlumnoDAO aluDao;
 	Alumno alu;
+	Alumno aluMod;
+	Alumno aluElim;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		 aluDao = new AlumnoDAO();
 		alu = new Alumno(0,"Lucia test", "Pereyra test", "luciapaulinapereyra@gmail.com", "java test", "github/luciapaulinapereyra");
+		aluMod= new Alumno(2,"Paulina ", "a test", "a@gmail.com", "java test", "github/a");
+		aluElim= new Alumno(1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 		aluDao = null;
 		alu = null;
+		aluMod=null;
+		aluElim=null;
 	}
 
-	@Test
-	void testAgregar() {
-		try {
-			aluDao.agregar(alu);
+//	@Test
+//	void testAgregar() {
+//		try {
+//			aluDao.agregar(alu);
+//			assertTrue(true);
+//		} catch (ClassNotFoundException | SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			assertTrue(false);
+//		}
+//	}
+	
+
+//     @Test
+//     void testModificar() {
+//    	 
+//    	 try {
+//			aluDao.modificar(aluMod);
+//			assertTrue(true);
+//		} catch (ClassNotFoundException | SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			assertTrue(false);
+//		}
+//    	 
+//     }
+     
+//     @Test
+//     void testEliminar() {
+//    	 try {
+//			aluDao.eliminar(aluElim);
+//			assertTrue(true);
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			assertTrue(false);
+//		}
+//     }
+     
+     @Test
+     void testLeerPorCodigo() {
+    	 try {
+			aluDao.leer(aluElim);
 			assertTrue(true);
-		} catch (ClassNotFoundException | SQLException e) {
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			assertTrue(false);
 		}
-	}
-	
-
- 
+     }
 
 }
 	
